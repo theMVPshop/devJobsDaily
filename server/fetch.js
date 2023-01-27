1cc19c30d4d0237003be29035a024d9cf1605883
taplinseth <taplinseth@gmail.com>
2023-01-26 19:11:08 -0600

    added key for remote jobs

--- server/fetch.js
+++ server/fetch.js
@@ -3,9 +3,9 @@ const pluginStealth = require ('puppeteer-extra-plugin-stealth');
 const {executablePath, ElementHandle} = require ('puppeteer');
 const axios = require ('axios');
 
-const experience = 'ENTRY_LEVEL';
+const experience = 'ENTRY_LEVEL'; // MID_LEVEL, SENIOR_LEVEL
 const location = 'austin';
-const remote = '';
+const remote = ''; //attr%28DSQF7%29 <----add for remote
 const searchTerm = 'javascript';
 const last24H = '&fromage=1';
 const url = `https://www.indeed.com/jobs?q=${searchTerm}&l=${location}&sc=0kf%3A${remote}explvl%28${experience}%29%3B&radius=50${last24H}&vjk=2b9775de01edc6d0`;
