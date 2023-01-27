0f067290b1425f249d75217755a7a6cd4c44ee14
kiffykowboy <cpgreene11@gmail.com>
2023-01-26 19:54:06 -0600

    basic layout

---
+++ devjobsdaily/src/bx1.js
@@ -0,0 +1,12 @@
+const Bx1 = () => {
+  return ( 
+    <section className="bx bx1">
+    <h1>Probs a graph or something</h1>
+          <div className="links">
+            <a href="./"> Home </a>
+          </div>
+    </section>
+   );
+}
+ 
+export default Bx1 ;
