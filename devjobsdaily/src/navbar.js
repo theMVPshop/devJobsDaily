5c05b956e29c0642a4ca59cda7dc8522dbe034dc
unknown <slisette127@gmail.com>
2023-01-31 19:54:11 -0600

    navbar

--- devjobsdaily/src/navbar.js
+++ devjobsdaily/src/navbar.js
@@ -1,14 +1,31 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Hamburger from './hamburger.js';
+
+
+
+
+
 const Navbar = () => {
   return ( 
-    <nav className="navbar">
-          <h1> Dev Jobs Daily </h1>
-          <div className="links">
-            <a href="./"> Home </a>
-          </div>
-
-
-    </nav>
-   );
+    
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+ 
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Menu
+        </Typography>
+        <button className='navbutton'>Login</button>
+      </Toolbar>
+    </AppBar>
+  </Box>
+ 
+        
+  );
 }
  
 export default Navbar;
