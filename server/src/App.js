c783093870d2f414b75107176807c380a78112cb
Jank <lmcgill36@gmail.com>
2023-02-14 19:11:51 -0600

    creating sign up component

--- server/src/App.js
+++ server/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import SearchForm from './SearchForm.JS';
+import SearchForm from './SearchForm';
+import SignUp from './signUp';
 
 function App() {
   return (
     <div className="App">
       <SearchForm />
+      <SignUp />
     </div>
   );
 }
