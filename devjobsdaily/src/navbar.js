3852de691b4750e645ded4b46a1914aa12d5e1be
kiffykowboy <cpgreene11@gmail.com>
2023-02-15 19:05:20 -0600

    some color stuff.

--- devjobsdaily/src/navbar.js
+++ devjobsdaily/src/navbar.js
@@ -1,14 +1,16 @@
 const Navbar = () => {
   return ( 
     <nav className="navbar">
+        <section className="bx bx4">
           <h1 className="navh1"> Dev Jobs Daily </h1>
-          <div className="links">
+        </section>
+          {/* <div className="links">
             <a href="./"> Link </a>
             <a href="./"> Link1 </a>
             <a href="./"> Link2 </a>
             <a href="./"> Link3 </a>
             <a href="./"> Link4 </a>
-          </div>
+          </div> */}
 
 
     </nav>
