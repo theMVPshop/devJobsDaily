3fe47e796472c14aa990c66d92871d080aa590c6
taplinseth <taplinseth@gmail.com>
2023-02-28 19:55:34 -0600

    delete and view functionality

--- server/src/App.js
+++ server/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import SearchBar from './components/SearchBar';
+import SearchBar from './components/SearchBar.js';
+import SearchList from './components/SearchList.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1 id='header'>Current Available Searches</h1>
       <SearchBar />
+      <SearchList />
     </div>
   );
 }
